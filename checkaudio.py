83eb3ce78fd1d8f874e2dcf6c263c963696d8614
ChrisG1015 <christophergustave10@gmail.com>
2020-02-03 18:04:45 -0500

    Added info on script

--- checkaudio.py
+++ checkaudio.py
@@ -5,17 +5,25 @@ import sys
 stdoutOrigin=sys.stdout 
 sys.stdout = open("audiolog.txt", "w")
 
+# use sr recognizer (easy to use, basic)
 r = sr.Recognizer()
 
+#functiong to process incoming audio
 incomingaudio = sr.AudioFile('OSR_us_000_0032_8k.wav')
 with incomingaudio as source:
     audio = r.record(source)
+
+# use the google_speech api via the speech_recognition wrapper to process the audio speech 
+print(r.recognize_google(audio))
+
+
+
+
 
 #print(type(audio))
 #print("-------------------")
 #print("\n")
 #print("-------------------")
 #print("\n")
-print(r.recognize_google(audio))
 #print("-------------------")
 #print("\n")
